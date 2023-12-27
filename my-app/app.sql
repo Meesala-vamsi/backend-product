0ed097469cb4c8c4cc38a2540d5bf033be970df4
vamsimeesala789@gmail.com <vamsimeesala789@gmail.com>
2023-12-27 18:01:47 +0530

    initial commit

--- my-app/app.sql
+++ my-app/app.sql
@@ -1,1 +1,14 @@
-SELECT * FROM book;
+-- CREATE TABLE user(
+-- username VARCHAR(100),
+-- email VARCHAR(100),password VARCHAR(100),
+-- verification_token VARCHAR(100)
+-- );
+
+-- INSERT INTO user(
+-- username,
+-- email,password
+-- )
+-- VALUES
+-- ('vamsi','vamsimeesala789@gmail.com','vamsisony');
+
+-- DELETE FROM user WHERE username='vamsi'
--- my-app/index.js
+++ my-app/index.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const path = require('path')
-const {open}=require('sqlite')
+const express = require('express')
 const sqlite3 = require('sqlite3')
-const dbPath = path.join(__dirname,'goodreads.db')
+const {open} = require('sqlite')
+const jwt = require('jsonwebtoken')
+const bcrypt=require('bcrypt')
+const path = require('path')
+const nodemailer = require('nodemailer');
+const {v4: uuidv4} = require('uuid')
+
+const dbPath = path.join(__dirname,'users.db')
 const app = express();
+
+let db=null 
+
 app.use(express.json())
 
-let db=null;
 
 const initializeDB = async ()=>{
     try{
@@ -23,50 +30,105 @@ const initializeDB = async ()=>{
 }
 }
 
-app.get('/books/:id',async(request,response)=>{
-    const {params} = request;
-    const {id} = params;
-    const getDetailsQuery=`
-        SELECT * FROM book WHERE book_id=${id};
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure:false,
+    auth: {
+        user: 'vamsimeesala789@gmail.com',
+        pass: 'cufi blvn ygwp dush'
+    },
+    authMethod: 'LOGIN'
+});
+
+function generateVerificationToken() {
+    return uuidv4(); 
+}
+
+
+const middleWare = (request,response,next)=>{
+    const getHead=request.headers['authorization']
+    let token = null
+    if(getHead!==undefined){
+        token = getHead.split(" ")[1]
+    }
+    if(token===undefined){
+        response.status(404)
+        response.send("Invalid JWT token")
+    }else{
+        jwt.verify(token,'vamsisony',async(error,user)=>{
+            if(error){
+                response.status(404);
+                response.send("Invalid JWT TOken")
+            }else{
+                next()
+            }
+        })
+    }
+}
+
+app.post('/login',async(request,response)=>{
+  const {username,password} = request.body 
+  const getUserStatusQuery = `SELECT * FROM user WHERE username='${username}'`
+  const getUserStatus= await db.get(getUserStatusQuery)
+  const hashedPassword= await bcrypt.hash(password,10)
+  
+  if(getUserStatus===undefined){
+    response.status(404);
+    response.send("Username Not Found")
+  }else{
+    const checkPasswordStatus=await bcrypt.compare(password,hashedPassword)
+    console.log(hashedPassword)
+    if(checkPasswordStatus===true){
+        const generateToken = jwt.sign({username},"vamsisony")
+        response.send(generateToken)
+    }else{
+        response.status(404);
+        response.send('Incorrect Password')
+    }
+  }
+})
+
+app.get('/',middleWare,async(request,response)=>{
+    response.send("Login Success")
+})
+
+app.post('/signin',async(request,response)=>{
+    const {password,username,email}= request.body
+
+    const verificationToken = generateVerificationToken();
+    const hashedPassword=await bcrypt.hash(password,10)
+    const addUserQuery = `
+    INSERT INTO user(
+        username,password,email,verification_token
+    )
+    VALUES
+    ('${username}','${hashedPassword}','${email}','${verificationToken}')
     `
-    const details = await db.get(getDetailsQuery);
-    response.send(details)
+    const addUser=await db.run(addUserQuery);
+    if (addUser!==undefined){
+            response.send("Username Already Exists!!")
+    } else {
+                // Create the verification link
+        const verificationLink = `https://yourwebsite.com/verify/${verificationToken}`;
+
+                // Send email with verification link to the provided email address
+        const mailOptions = {
+            from: 'vamsimeesala2003@gmail.com',
+            to: email,
+            subject: 'Email Verification',
+            html: `<p>Click <a href="${verificationLink}">here</a> to verify your email.</p>`
+        };
+
+        transporter.sendMail(mailOptions, (mailErr, info) => {
+            if (mailErr) {
+                console.error('Error sending email:', mailErr);
+                response.status(500).send('Error sending verification email');
+            } else {
+                console.log('Email sent:', info.response);
+                response.status(200).send('Registration successful. Please verify your email.');
+            }
+        });
+    }
 })
 
-app.post('/books/',async(request,response)=>{
-    const bookDetails=request.body
-    const {title,authorId,rating,ratingCount,reviewCount,description,pages,dateOfPublication,editionLanguage,price,onlineStores} = bookDetails
-    const addBooksQuery=`
-        INSERT INTO book (
-            title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
-            VALUES(
-                '${title}',
-                ${authorId},
-                ${rating},
-                ${ratingCount},
-                ${reviewCount},
-                '${description}',
-                ${pages},
-                '${dateOfPublication}',
-                '${editionLanguage}',
-                '${price}',
-                '${onlineStores}'
-            )
-          
-            
-    `
-    const getDetails=await db.run(addBooksQuery)
-    const bookId = getDetails.lastID
-    response.send({bookId:bookId})
-    
-})
-
-app.get('/',async(request,response)=>{
-    const getDetailsQuery = `SELECT * FROM book`
-
-    const responseData = await db.all(getDetailsQuery)
-    response.send(responseData)
-})
-
-initializeDB();
-
+initializeDB()
